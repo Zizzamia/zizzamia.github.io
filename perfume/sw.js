9657b4ca20628350b7cf70191c4e2089751ac2ac
Leonardo Zizzamia <leonardo.zizzamia@coinbase.com>
2020-05-16 11:35:45 -0700

    sw

--- perfume/sw.js
+++ perfume/sw.js
@@ -1,1 +1,38 @@
-console.log('ciao');
+const handleResponse = async (event, request) => {
+  const cache = await caches.open('perfume-cache');
+  const responseCached = await cache.match(request);
+  event.waitUntil(
+    (async function () {
+      const responseFetched = await fetch(request);
+      if (!responseFetched.clone) {
+        return;
+      }
+      const responseCloned = responseFetched.clone();
+      if (!responseCloned || responseClone.status !== 200) {
+        return;
+      }
+      await cache.put(request, responseCloned);
+    })(),
+  );
+  if (responseCached) {
+    return responseCached;
+  }
+  return fetch(request);
+};
+
+self.addEventListener('fetch', async event => {
+  console.log('event', event.request.url, new URL(request.url));
+  // event.respondWith(
+  //   (async function () {
+  //     return await handleResponse(event, event.request);
+  //   })(),
+  // );
+});
+
+self.addEventListener('install', async () => {
+  await self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  self.clients.claim();
+});
